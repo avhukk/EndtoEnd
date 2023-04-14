07f0babe4c4fe7d4a4791e635d8471a32962dbf6
avhukk <avhukkerikar@gmail.com>
2023-04-14 20:51:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## End to End ML project
+### Created an Environment
+```
+conda create -p env python==3.8
+
+conda activate venv/
+```
+### Install all necessary libraries
+```
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,20 @@
+## To convert the folders into packages
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]: 
+    requirements = []
+    with open(file_path) as file_obj: 
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+    return requirements
+
+setup (
+    name = 'RegressorProject', 
+    version = '0.0.1', 
+    author = 'Abhishek', 
+    author_email= 'avhukkerikar@gmail.com', 
+    install_requires = ['numpy', 'pandas', 'matplotlib', 'Flask'],
+    packages = find_packages()
+)
