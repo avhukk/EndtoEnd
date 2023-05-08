6f5020764a521e1459731db4c7928582a53493bb
avhukk <avhukkerikar@gmail.com>
2023-05-08 15:19:06 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from src.exception import CustomException
 from src.logger import logging
 
+## Saving a file
 def save_object(file_path, obj):
     try: 
         dir_path = os.path.dirname(file_path)
@@ -15,4 +16,36 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
     except Exception as e: 
         raise CustomException(e,sys)
+    
+## Evaluating model
+def evaluate_model(X_train, y_train, X_test, y_test, models): 
+    try: 
+        report = {}
+        for i in range(len(models)): 
+            model = list(models.values())[i]
+            # Train the model
+            model.fit(X_train, y_train)
+            # Predict Test day 
+            y_test_pred = model.predict(X_test)
+            # Getting R2 values for train and test data
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e: 
+        logging.info('Error occured while training model')
+        raise CustomException(e,sys)
+    
+    def load_object(file_path):
+        try: 
+            with open(file_path, 'rb') as file_obj: 
+                return pickle.load(file_obj)
+        except Exception as e: 
+            logging.info("Exception occured in load_object function")
+            raise CustomException(e,sys)
+    
+
+
+
+
+
 
